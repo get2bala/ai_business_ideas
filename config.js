2282e931a040154964f6859873b5d1625949f13b
get2bala <get2bala@gmail.com>
2025-08-29 22:17:12 -0700

    V1 initial layout with database read

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+// Store sensitive credentials here. Do NOT commit this file to public repositories.
+
+const SUPABASE_URL = 'https://gdpjavidjguhxzaaqldi.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkcGphdmlkamd1aHh6YWFxbGRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY1Mjc1MzAsImV4cCI6MjA3MjEwMzUzMH0.V1ZQOguQkni0OfA-2hkGclAWQl9wPkEBvcdG65nwYWk';
